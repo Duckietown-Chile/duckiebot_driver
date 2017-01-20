ca13b15ce0e63b61597f4ad735e8dd2f78c98782
Rodrigo Muñoz <rorro.mr@gmail.com>
2017-01-20 12:54:52 -0300

    PWM Test

--- scripts/test.py
+++ scripts/test.py
@@ -8,21 +8,22 @@ def test_blink(duck):
     cmd = DuckietownCommand()
     status = DuckietownStatus()
     for i in range(20):
-        cmd.led = False
-        cmd.pwm_ch2 = -200+i*20
+        cmd.led = not cmd.led
+        cmd.pwm_ch1 = -200+i*20
         duck.send_command(cmd)
-        duck.get_status(status)
-        print status
-        time.sleep(0.1)
-        cmd.led = True
-        duck.send_command(cmd)
-        duck.get_status(status)
-        print status
+        print cmd.pwm_ch1
+        # duck.get_status(status)
+        # print status
+        # time.sleep(0.1)
+        # cmd.led = True
+        # duck.send_command(cmd)
+        # duck.get_status(status)
+        # print status
         time.sleep(0.1)
     
 def main():
     duck = DuckietownSerial('/dev/ttyUSB0', baudrate = 57600)
-    time.sleep(4.0) # for arduino bootloader
+    time.sleep(2.0) # for arduino bootloader
     test_blink(duck)
             
 if __name__ == '__main__':
